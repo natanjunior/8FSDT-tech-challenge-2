ccccd106293dccf3f752f82ca4b04f7ee901e493
natanjunior <natan.dsjr@gmail.com>
2026-06-05 19:34:23 -0300

    feat(model): refactor User (login, password_hash, hide hash by default)

--- src/models/User.js
+++ src/models/User.js
@@ -3,85 +3,64 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-	class User extends Model {
-		static associate(models) {
-			// User hasMany Posts (as author)
-			User.hasMany(models.Post, {
-				foreignKey: 'author_id',
-				as: 'posts'
-			});
+  class User extends Model {
+    static associate(models) {
+      User.hasOne(models.Teacher, {
+        foreignKey: 'user_id',
+        as: 'teacher'
+      });
 
-			// User hasMany PostReads
-			User.hasMany(models.PostRead, {
-				foreignKey: 'user_id',
-				as: 'post_reads'
-			});
+      User.hasOne(models.Student, {
+        foreignKey: 'user_id',
+        as: 'student'
+      });
 
-			// User hasMany UserSessions
-			User.hasMany(models.UserSession, {
-				foreignKey: 'user_id',
-				as: 'sessions'
-			});
-		}
-	}
+      User.hasMany(models.UserSession, {
+        foreignKey: 'user_id',
+        as: 'sessions'
+      });
+    }
+  }
 
-	User.init(
-		{
-			id: {
-				type: DataTypes.UUID,
-				defaultValue: DataTypes.UUIDV4,
-				primaryKey: true,
-				allowNull: false
-			},
-			name: {
-				type: DataTypes.STRING(255),
-				allowNull: false,
-				validate: {
-					notEmpty: {
-						msg: 'Nome é obrigatório'
-					},
-					len: {
-						args: [3, 255],
-						msg: 'Nome deve ter entre 3 e 255 caracteres'
-					}
-				}
-			},
-			email: {
-				type: DataTypes.STRING(255),
-				allowNull: false,
-				unique: {
-					name: 'users_email_unique',
-					msg: 'Email já cadastrado'
-				},
-				validate: {
-					isEmail: {
-						msg: 'Email inválido'
-					},
-					notEmpty: {
-						msg: 'Email é obrigatório'
-					}
-				}
-			},
-			role: {
-				type: DataTypes.ENUM('TEACHER', 'STUDENT'),
-				allowNull: false,
-				defaultValue: 'STUDENT',
-				validate: {
-					isIn: {
-						args: [['TEACHER', 'STUDENT']],
-						msg: 'Role deve ser TEACHER ou STUDENT'
-					}
-				}
-			}
-		},
-		{
-			sequelize,
-			modelName: 'User',
-			tableName: 'users',
-			underscored: true,
-			timestamps: true
-		}
-	);
+  User.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false
+      },
+      login: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: {
+          name: 'users_login_unique',
+          msg: 'Login já cadastrado'
+        }
+      },
+      password_hash: {
+        type: DataTypes.STRING(60),
+        allowNull: false
+      },
+      role: {
+        type: DataTypes.ENUM('TEACHER', 'STUDENT'),
+        allowNull: false
+      }
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'users',
+      underscored: true,
+      timestamps: true,
+      defaultScope: {
+        attributes: { exclude: ['password_hash'] }
+      },
+      scopes: {
+        withPassword: { attributes: { include: ['password_hash'] } }
+      }
+    }
+  );
 
-	return User;
+  return User;
 };
