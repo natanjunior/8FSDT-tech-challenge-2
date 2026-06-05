801135baa240977af9572cd2a5c48dac4fc59a7d
natanjunior <natan.dsjr@gmail.com>
2026-06-05 19:43:58 -0300

    refactor(repo): user.repository works with login+password_hash

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,11 +1,34 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
 class UserRepository {
-	async findByEmail(email) {
-		return User.findOne({ where: { email } });
-	}
+  async findByLogin(login) {
+    return User.scope('withPassword').findOne({ where: { login } });
+  }
+
+  async findById(id) {
+    return User.findByPk(id);
+  }
+
+  async create({ login, password, role }, options = {}) {
+    const password_hash = await bcrypt.hash(password, 10);
+    return User.create({ login, password_hash, role }, options);
+  }
+
+  async updatePasswordHash(id, password) {
+    const password_hash = await bcrypt.hash(password, 10);
+    return User.update({ password_hash }, { where: { id } });
+  }
+
+  async delete(id, options = {}) {
+    return User.destroy({ where: { id }, ...options });
+  }
+
+  async verifyPassword(plain, hash) {
+    return bcrypt.compare(plain, hash);
+  }
 }
 
 module.exports = UserRepository;
